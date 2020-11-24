c49d751cda8e87221b6eda33ad380f00e278398b
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-24 23:05:16 +0000

    Do not enforce integrity header

--- test/react-native/features/support/env.rb
+++ test/react-native/features/support/env.rb
@@ -1,3 +1,9 @@
+AfterConfiguration do |_config|
+  MazeRunner.config.receive_no_requests_wait = 15
+  MazeRunner.config.receive_requests_wait = 15
+  MazeRunner.config.enforce_bugsnag_integrity = false
+end
+
 Before('@android_only') do |scenario|
   skip_this_scenario("Skipping scenario") if MazeRunner.driver.capabilities["os"] == 'ios'
 end
