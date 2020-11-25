de2aa9db57592abc12f4b844dcee8af7ca84c84e
Steve Kirkland <twometresteve@users.noreply.github.com>
2020-11-25 09:40:52 +0000

    Wait 30s for no requests

--- test/react-native/features/support/env.rb
+++ test/react-native/features/support/env.rb
@@ -1,6 +1,6 @@
 AfterConfiguration do |_config|
-  MazeRunner.config.receive_no_requests_wait = 15
-  MazeRunner.config.receive_requests_wait = 15
+  MazeRunner.config.receive_no_requests_wait = 30
+  MazeRunner.config.receive_requests_wait = 30
   MazeRunner.config.enforce_bugsnag_integrity = false
 end
 
