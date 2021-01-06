02d75d489012437cec86337609f001dcdfaa02b1
Steve Kirkland <twometresteve@users.noreply.github.com>
2021-01-06 07:15:09 +0000

    Debug build script

--- test/react-native-cli/features/fixtures/build-ios.sh
+++ test/react-native-cli/features/fixtures/build-ios.sh
@@ -4,27 +4,43 @@ then
   exit 1
 fi
 
-cd $1 || exit 1
+echo 'DEBUG: Running build-ios.sh'
 
-rm -rf "$1.xcarchive"
+cd rn0_60 || exit 1
+
+echo 'DEBUG: rm -rf'
+
+rm -rf "rn0_60.xcarchive"
 
 cd ios || exit 1
+
+echo 'DEBUG: pod install'
+
 pod install || pod install --repo-update
-xcrun xcodebuild \
-  -scheme "$1" \
-  -workspace "$1.xcworkspace" \
+
+echo 'DEBUG: xcodebuild 1'
+
+xcrun --log xcodebuild \
+  -scheme "rn0_60" \
+  -workspace "rn0_60.xcworkspace" \
   -configuration Release \
-  -archivePath "../$1.xcarchive" \
+  -archivePath "../rn0_60.xcarchive" \
   -allowProvisioningUpdates \
-  -quiet \
+  -verbose \
   archive
+echo "exit: $?"
 
-cd ..
+cd ../..
 
-xcrun xcodebuild -exportArchive \
-  -archivePath "$1.xcarchive" \
-  -exportPath ../output/ \
-  -quiet \
-  -exportOptionsPlist ../exportOptions.plist
+echo 'DEBUG: xcodebuild 2'
+ls -l
+echo "In: $PWD"
 
-rm -rf "$1.xcarchive"
+xcrun --log xcodebuild -exportArchive \
+  -archivePath "rn0_60/rn0_60.xcarchive" \
+  -exportPath output \
+  -verbose \
+  -exportOptionsPlist exportOptions.plist
+echo "exit: $?"
+
+#rm -rf "rn0_60.xcarchive"
